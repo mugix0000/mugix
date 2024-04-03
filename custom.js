88d15b4884b9ae4ee853dcc537533f1d132a45ce
mugix <mugix0000@gmail.com>
2024-04-04 00:00:08 +0900

    area1

--- custom.js
+++ custom.js
@@ -1,3 +1,4 @@
+//メニューボタン
 $(function () {
     $("#js-hamburger-menu, .navigation_link").on("click", function () {
       $(".navigation").slideToggle(100)
@@ -10,4 +11,51 @@ $(function () {
         $(".header-color").addClass("header-color-open")
       }
     });
+});
+//横からスライド
+$(function(){
+  $(".inview").on("inview", function (event, isInView) {
+    if (isInView) {
+      $(this).stop().addClass("is-show");
+    } else {
+      $(this).stop().removeClass("is-show")
+    }
   });
+});
+//背景色変更
+$(function(){
+  function changeColor(color){
+    $("body").stop().css("background-color", color);
+  }
+  $("#white").on("inview", function (event, isInView) {
+    if (isInView) {
+      changeColor("white")
+    }
+  });
+  $("#yellow").on("inview", function (event, isInView) {
+    if (isInView) {
+      changeColor("yellow")
+    }
+  });
+  $("#green").on("inview", function (event, isInView) {
+    if (isInView) {
+      changeColor("green")
+    }
+  });
+  $("#cyan").on("inview", function (event, isInView) {
+    if (isInView) {
+      changeColor("cyan")
+    }
+  });
+  $("#blue").on("inview", function (event, isInView) {
+    if (isInView) {
+      changeColor("blue")
+    }
+  });
+  $("#grey").on("inview", function (event, isInView) {
+    if (isInView) {
+      changeColor("lightgrey")
+    }
+  });
+});
+
