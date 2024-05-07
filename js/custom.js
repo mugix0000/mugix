37080cbb0e3f211891d34a5d8540f5060fbe9e2c
mugix <mugix0000@gmail.com>
2024-05-08 01:13:25 +0900

    contact

--- js/custom.js
+++ js/custom.js
@@ -28,6 +28,25 @@ $(function(){
     }
   });
 });
+//個別のInview
+$(function(){
+  $("#title").on("inview", function (event, isInView) {
+    if (isInView) {
+      $(this).stop().addClass("is-appear");
+      $(".inview-wani").stop().addClass("wani");
+      $(".inview-kirin").stop().addClass("kirin");
+    }
+  });
+});
+$(function(){
+  $("#about-us").on("inview", function (event, isInView) {
+    if (isInView) {
+      $(this).stop().addClass("is-appear");
+      $(".inview-rakko").stop().addClass("rakko");
+    }
+  });
+});
+
 var omikujiOn = false;
 //むぎのセリフ
 $(function(){
@@ -74,6 +93,70 @@ $(window).on("scroll", function(){
     $(".climber").css("background-position", "-240px 0");
     isScrolling = false;
   }, 500);
+});
+//ハチ
+var beeLeft = 0;
+var beeTop = 0;
+var flowers = ["#flower1", "#flower2", "#flower3", "#flower4"];
+$(function(){
+  beeLeft = $("#flower1").offset().left + 30;
+  beeTop = $("#flower1").offset().top + 30;
+  $("#bee").css({top:beeTop, left:beeLeft});
+});
+setInterval(function(){
+  var index = Math.floor(Math.random() * flowers.length);
+  beeLeft = $(flowers[index]).offset().left + 30;
+  beeTop = $(flowers[index]).offset().top + 30;
+  if ($("#bee").offset().left < beeLeft) {
+    $("#bee").css("transform", "scaleX(-1)");
+  }else{
+    $("#bee").css("transform", "scaleX(1)");
+  };
+  $("#bee").animate({top:beeTop, left:beeLeft}, 5000, "swing");
+}, 7000);
+//あんちゃんのゲーム
+var annButtons = ["#ann-game1", "#ann-game2", "#ann-game3", "#ann-game4"];
+var ocelos = [false, false, false, false];
+$(function(){
+  oceloOn = function(int){
+    ocelos[int] = true;
+    $(annButtons[int]).css({"box-shadow": "0px 0px 0 #7a5143", "transform": "translateY(12px)"});
+  };
+  oceloOff = function(int){
+    ocelos[int] = false;
+    $(annButtons[int]).css({"box-shadow": "0px 12px 0 #7a5143", "transform": "translateY(0)"});
+  };
+  judge = function(){
+    if(ocelos.every(element => element === true)){
+      $("#annchan").css("right", "0");
+    }else{
+      $("#annchan").css("right", "-120%");
+    }
+  };
+  $("#ann-game1").on("click", function(){
+    (ocelos[0]) ? oceloOff(0) : oceloOn(0);
+    (ocelos[1]) ? oceloOff(1) : oceloOn(1);
+    (ocelos[2]) ? oceloOff(2) : oceloOn(2);
+    judge();
+  });
+  $("#ann-game2").on("click", function(){
+    (ocelos[0]) ? oceloOff(0) : oceloOn(0);
+    (ocelos[1]) ? oceloOff(1) : oceloOn(1);
+    (ocelos[3]) ? oceloOff(3) : oceloOn(3);
+    judge();
+  });
+  $("#ann-game3").on("click", function(){
+    (ocelos[0]) ? oceloOff(0) : oceloOn(0);
+    (ocelos[3]) ? oceloOff(3) : oceloOn(3);
+    (ocelos[2]) ? oceloOff(2) : oceloOn(2);
+    judge();
+  });
+  $("#ann-game4").on("click", function(){
+    (ocelos[3]) ? oceloOff(3) : oceloOn(3);
+    (ocelos[1]) ? oceloOff(1) : oceloOn(1);
+    (ocelos[2]) ? oceloOff(2) : oceloOn(2);
+    judge();
+  });
 });
 //スライダー
 $(function(){
