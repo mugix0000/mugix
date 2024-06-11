54d5112b9ea336a299944d3412a535c33ac9b0cf
mugix <mugix0000@gmail.com>
2024-06-12 00:12:42 +0900

    11_2

--- js/mugix.js
+++ js/mugix.js
@@ -1,3 +1,4 @@
+//戻るボタン
 $(function(){
   $(".return").on("click", function(){
     if(window.history.length > 1) {
@@ -7,3 +8,48 @@ $(function(){
     }
   });
 })
+//Inview
+$(function(){
+  $(".inview").on("inview", function (event, isInView) {
+    if (isInView) {
+      $(this).stop().addClass("is-show");
+    }
+  });
+});
+//Inview-Menu
+$(function(){
+  $("#menu").on("inview", function (event, isInView) {
+    if (isInView) {
+      var children = $(this).children();
+      function menuOpen(index) {
+        if(index < children.length) {
+          var child = $(children[index]);
+          child.stop().addClass("is-show");
+          setTimeout(function(){
+            menuOpen(index + 1);
+          }, 200);
+        }
+      }
+      menuOpen(0);
+    }
+  });
+});
+//Inview-status
+$(function(){
+  $(".status-area").on("inview", function (event, isInView) {
+    if (isInView) {
+      var children = $(this).children();
+      function menuOpen(index) {
+        if(index < children.length) {
+          var child = $(children[index]);
+          child.stop().addClass("is-show");
+          setTimeout(function(){
+            menuOpen(index + 1);
+          }, 100);
+        }
+      }
+      menuOpen(0);
+    }
+  });
+});
+
